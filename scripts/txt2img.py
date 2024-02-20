2cf5b87b651e9ce8140a0b777ec9987abfe3d67c
hojunking <hojunking3@gmail.com>
2024-02-20 22:13:07 +0900

    Add stablediffusion-v2

--- scripts/txt2img.py
+++ scripts/txt2img.py
@@ -243,9 +243,17 @@ def main(opt):
     else:
         print(f"reading prompts from {opt.from_file}")
         with open(opt.from_file, "r") as f:
-            data = f.read().splitlines()
-            data = [p for p in data for i in range(opt.repeat)]
-            data = list(chunk(data, batch_size))
+            #data = f.read().splitlines()
+            #data = [p for p in data for i in range(opt.repeat)]
+            f_str = f.read()
+            s_str = f_str.split(',')
+            str_list = [s_str[i].strip(" '") for i in range(len(s_str))]
+           
+            #after_trans = translate(before_trans)
+            print(f"str_list len :{len(str_list)}")
+            #after_trans = str_list
+
+            data = list(chunk(str_list, batch_size))
 
     sample_path = os.path.join(outpath, "samples")
     os.makedirs(sample_path, exist_ok=True)
@@ -256,79 +264,6 @@ def main(opt):
     start_code = None
     if opt.fixed_code:
         start_code = torch.randn([opt.n_samples, opt.C, opt.H // opt.f, opt.W // opt.f], device=device)
-
-    if opt.torchscript or opt.ipex:
-        transformer = model.cond_stage_model.model
-        unet = model.model.diffusion_model
-        decoder = model.first_stage_model.decoder
-        additional_context = torch.cpu.amp.autocast() if opt.bf16 else nullcontext()
-        shape = [opt.C, opt.H // opt.f, opt.W // opt.f]
-
-        if opt.bf16 and not opt.torchscript and not opt.ipex:
-            raise ValueError('Bfloat16 is supported only for torchscript+ipex')
-        if opt.bf16 and unet.dtype != torch.bfloat16:
-            raise ValueError("Use configs/stable-diffusion/intel/ configs with bf16 enabled if " +
-                             "you'd like to use bfloat16 with CPU.")
-        if unet.dtype == torch.float16 and device == torch.device("cpu"):
-            raise ValueError("Use configs/stable-diffusion/intel/ configs for your model if you'd like to run it on CPU.")
-
-        if opt.ipex:
-            import intel_extension_for_pytorch as ipex
-            bf16_dtype = torch.bfloat16 if opt.bf16 else None
-            transformer = transformer.to(memory_format=torch.channels_last)
-            transformer = ipex.optimize(transformer, level="O1", inplace=True)
-
-            unet = unet.to(memory_format=torch.channels_last)
-            unet = ipex.optimize(unet, level="O1", auto_kernel_selection=True, inplace=True, dtype=bf16_dtype)
-
-            decoder = decoder.to(memory_format=torch.channels_last)
-            decoder = ipex.optimize(decoder, level="O1", auto_kernel_selection=True, inplace=True, dtype=bf16_dtype)
-
-        if opt.torchscript:
-            with torch.no_grad(), additional_context:
-                # get UNET scripted
-                if unet.use_checkpoint:
-                    raise ValueError("Gradient checkpoint won't work with tracing. " +
-                    "Use configs/stable-diffusion/intel/ configs for your model or disable checkpoint in your config.")
-
-                img_in = torch.ones(2, 4, 96, 96, dtype=torch.float32)
-                t_in = torch.ones(2, dtype=torch.int64)
-                context = torch.ones(2, 77, 1024, dtype=torch.float32)
-                scripted_unet = torch.jit.trace(unet, (img_in, t_in, context))
-                scripted_unet = torch.jit.optimize_for_inference(scripted_unet)
-                print(type(scripted_unet))
-                model.model.scripted_diffusion_model = scripted_unet
-
-                # get Decoder for first stage model scripted
-                samples_ddim = torch.ones(1, 4, 96, 96, dtype=torch.float32)
-                scripted_decoder = torch.jit.trace(decoder, (samples_ddim))
-                scripted_decoder = torch.jit.optimize_for_inference(scripted_decoder)
-                print(type(scripted_decoder))
-                model.first_stage_model.decoder = scripted_decoder
-
-        prompts = data[0]
-        print("Running a forward pass to initialize optimizations")
-        uc = None
-        if opt.scale != 1.0:
-            uc = model.get_learned_conditioning(batch_size * [""])
-        if isinstance(prompts, tuple):
-            prompts = list(prompts)
-
-        with torch.no_grad(), additional_context:
-            for _ in range(3):
-                c = model.get_learned_conditioning(prompts)
-            samples_ddim, _ = sampler.sample(S=5,
-                                             conditioning=c,
-                                             batch_size=batch_size,
-                                             shape=shape,
-                                             verbose=False,
-                                             unconditional_guidance_scale=opt.scale,
-                                             unconditional_conditioning=uc,
-                                             eta=opt.ddim_eta,
-                                             x_T=start_code)
-            print("Running a forward pass for decoder")
-            for _ in range(3):
-                x_samples_ddim = model.decode_first_stage(samples_ddim)
 
     precision_scope = autocast if opt.precision=="autocast" or opt.bf16 else nullcontext
     with torch.no_grad(), \
@@ -360,24 +295,24 @@ def main(opt):
                     for x_sample in x_samples:
                         x_sample = 255. * rearrange(x_sample.cpu().numpy(), 'c h w -> h w c')
                         img = Image.fromarray(x_sample.astype(np.uint8))
-                        img = put_watermark(img, wm_encoder)
+                        #img = put_watermark(img, wm_encoder)
                         img.save(os.path.join(sample_path, f"{base_count:05}.png"))
                         base_count += 1
                         sample_count += 1
 
-                    all_samples.append(x_samples)
+                    #all_samples.append(x_samples)
 
             # additionally, save as grid
-            grid = torch.stack(all_samples, 0)
-            grid = rearrange(grid, 'n b c h w -> (n b) c h w')
-            grid = make_grid(grid, nrow=n_rows)
+            # grid = torch.stack(all_samples, 0)
+            # grid = rearrange(grid, 'n b c h w -> (n b) c h w')
+            # grid = make_grid(grid, nrow=n_rows)
 
-            # to image
-            grid = 255. * rearrange(grid, 'c h w -> h w c').cpu().numpy()
-            grid = Image.fromarray(grid.astype(np.uint8))
-            grid = put_watermark(grid, wm_encoder)
-            grid.save(os.path.join(outpath, f'grid-{grid_count:04}.png'))
-            grid_count += 1
+            # # to image
+            # grid = 255. * rearrange(grid, 'c h w -> h w c').cpu().numpy()
+            # grid = Image.fromarray(grid.astype(np.uint8))
+            # grid = put_watermark(grid, wm_encoder)
+            # grid.save(os.path.join(outpath, f'grid-{grid_count:04}.png'))
+            # grid_count += 1
 
     print(f"Your samples are ready and waiting for you here: \n{outpath} \n"
           f" \nEnjoy.")
